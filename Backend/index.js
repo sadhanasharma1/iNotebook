920e365abcba72f1b5e39576e1954c89976d07bf
Sadhana Sharma <sadhanaksharma6@gmail.com>
2024-06-01 12:29:50 +0530

    Adding data into database

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+//we can write all routes here as well but it will be messy so better create sseperate file for routes and connect iit here.
+
+//Available routes:
+//.use can be used with one parameter as well i.e just by giving file location:
+//here par1 : route path & par2: file name to be used
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
