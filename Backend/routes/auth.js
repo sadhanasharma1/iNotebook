8555bcd9646fb7780886028914e5a83e0933db61
Sadhana Sharma <sadhanaksharma6@gmail.com>
2024-06-02 11:24:36 +0530

    bcrypt used to store the hash of a password after addig salt to it. Then jwttoken implementation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express= require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 const router=express.Router();
 
-
+const JWT_SECRET = 'sadhanaisgoodgi$rl'
 //Create a User using POST "/api/auth/createuser". No Login Required
 router.post('/createuser',[
     body('name','Enter a valid name').isLength({min:5}),
@@ -21,15 +23,23 @@ router.post('/createuser',[
 if(user){
     return res.status(400).json({error: "sorry a user with this email already exist"})
 }
+const salt=  await bcrypt.genSalt(10);  // salt generated
 
+const secPass= await bcrypt.hash(req.body.password,salt);
+//Create a new user
  user= await User.create({
         name:req.body.name,
-        password:req.body.password,
+        password:secPass,
         email:req.body.email
-    })
-
-
-    res.json(user)}
+    });
+   const data={
+    user:{
+        id:user.id
+    }
+   }
+   const authToken = jwt.sign(data,JWT_SECRET);
+    res.json({authToken})
+}
     catch(err){
         console.log(err.message);
         res.status(500).send("some error has occured");
