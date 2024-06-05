88b695d6f346f7864a18ade0615998c9a64f75dd
Sadhana Sharma <sadhanaksharma6@gmail.com>
2024-06-05 18:05:56 +0530

    Appliccation of useContext and how components can directly just communicate wiht useContext, without any propdrill

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
+import About from './Components/About' 
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Routes,
+  Route
 } from "react-router-dom";
+import NoteState from './context/notes/NoteState';
 function App() {
   return (
+    //NoteSatte me wrap krne ke baad NoteState me jitne bhi state
+    //variable h vo sbb is andar wale compnents koa ccessible ho jaega.
+    // So no any prop drill needed just a dtrect talk between the component and the context
     <>
+    <NoteState>
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />}>
+          <Route exact path="/" element={<Home/>}>
           </Route>
-          <Route exact path="/about" element={<About />}>
+          <Route exact path="/about" element={<About/>}>
           </Route>
         </Routes>
       </Router>
-
+      </NoteState>
     </>
   );
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useContext,useEffect} from 'react'
+import noteContext from '../context/notes/noteContext';
 
 export default function About() {
+  const a = useContext(noteContext);
+  useEffect(()=>{
+    a.update()
+  },[])
   return (
     <div>
-      <h1>This is About</h1>
+      <h1>This is About {a.state.name} and he is in class {a.state.class}</h1>
     </div>
   )
 }
