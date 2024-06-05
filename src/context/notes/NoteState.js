8630594736936c9ba9a56641e34afe6dde14313c
Sadhana Sharma <sadhanaksharma6@gmail.com>
2024-06-05 19:24:21 +0530

    manuall few hard coded notes embeeded and shown into the page

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,12 +1,35 @@
+import { UNSAFE_DataRouterStateContext } from "react-router-dom";
 import NoteContext from "./noteContext";
 import {useState} from "react";
 const NoteState = (props) => {
+    const notesInitial= [
+        {
+          "_id": "665cb0b0dae3bb2548dcb1df",
+          "user": "665c7160a176950ef2e3e445",
+          "title": "New Updated note",
+          "description": "my-description",
+          "tag": "first note personal",
+          "date": "2024-06-02T17:49:36.020Z",
+          "__v": 0
+        },
+        {
+          "_id": "665cb0f4dae3bb2548dcb1e6",
+          "user": "665c7160a176950ef2e3e445",
+          "title": "my-title2",
+          "description": "my-description2",
+          "tag": "first note personal2",
+          "date": "2024-06-02T17:50:44.433Z",
+          "__v": 0
+        }
+      ];
+
+      const [notes,setNotes]= useState(notesInitial)
 
     return (
         // jo bhi value pass krni h usee value={ } ke andar daal do
         // jabhi kisi cheezo ko hm ye ctreated context (NoteContext) ke andar wrap krenge to
         //automatically sare children aa jaenbge   
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
